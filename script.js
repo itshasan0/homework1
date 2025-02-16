dc7e6035c9adb235eb52dec9b40415ec02266e94
itshasan0 <hasan1shekary@gmail.com>
2025-02-17 01:11:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let word = 'abcde';
+console.log(word[0]); 
+console.log(word[1]); 
+console.log(word[4]); 
+
+
+let firstNumber = parseInt(prompt("Enter number 1:"));
+let secondNumber = parseInt(prompt("Enter number 2:"));
+console.log(firstNumber > 0 || secondNumber > 0); 
+
+
+let age = parseInt(prompt("Enter your age:"));
+if (age >= 18) {
+    console.log("Adult"); 
+} else {
+    console.log("Minor"); 
+}
